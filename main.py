51d7acbdf855d67a34d3b10d0420d39a6d8e9f18
Jack Evans <aquatically123@gmail.com>
2024-09-02 16:48:28 +0100

    Problem occured with last commit, this should work

--- main.py
+++ main.py
@@ -8,18 +8,27 @@ from wtforms import StringField
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Thisisasecret!"
 
+def indexFiles():
+    fileList = []
+    for target in ["index.html", "style.html"]:
+        try:
+            with open(target, "r") as file:
+                fileList.append(file.read())
+        except FileNotFoundError:
+            exit("index.html not found")
+    return(fileList)
+
 @app.route("/")
 def head():
     return redirect("/fuelBased")
 
 @app.route("/fuelBased", methods=["GET", "POST"])
 def fuelBased():
-    with open("index.html", "r") as file:
-        index = file.read()
+    index, style = indexFiles()
     class InputForm(FlaskForm):
-        fuel = StringField("fuel")
-        plastic = StringField("plastic")
-        rubber = StringField("rubber")
+        fuel = StringField("Fuel", render_kw={"placeholder": "Fuel"})
+        plastic = StringField("Plastic", render_kw={"placeholder": "Plastic"})
+        rubber = StringField("Rubber", render_kw={"placeholder": "Rubber"})
     form = InputForm()
     if form.validate_on_submit():
         print(form.fuel.data, form.plastic.data, form.rubber.data)
@@ -29,20 +38,21 @@ def fuelBased():
                                plasticRefineries=plasticRefineries, 
                                rubberRefineries=rubberRefineries, 
                                response=True, 
-                               index=index)
+                               index=index,
+                               style=style)
     return render_template("fuelBased.html", 
                            form=form, 
                            response=False, 
-                           index=index)
+                           index=index,
+                           style=style)
 
 @app.route("/horBased", methods=["GET", "POST"])
 def horBased():
-    with open("index.html", "r") as file:
-        index = file.read()
+    index, style = indexFiles()
     class InputForm(FlaskForm):
-        hor = StringField("hor")
-        plastic = StringField("plastic")
-        rubber = StringField("rubber")
+        hor = StringField("HOR", render_kw={"placeholder": "HOR"})
+        plastic = StringField("Plastic", render_kw={"placeholder": "Plastic"})
+        rubber = StringField("Rubber", render_kw={"placeholder": "Rubber"})
     form = InputForm()
     if form.validate_on_submit():
         print(form.hor.data, form.plastic.data, form.rubber.data)
@@ -52,11 +62,13 @@ def horBased():
                                plasticRefineries=plasticRefineries, 
                                rubberRefineries=rubberRefineries, 
                                response=True, 
-                               index=index)
+                               index=index,
+                               style=style)
     return render_template("horBased.html", 
                            form=form, 
                            response=False, 
-                           index=index)
+                           index=index,
+                           style=style)
 
 if __name__ == "__main__":
     app.run(debug=True)
